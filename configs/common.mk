f21a167fc8478dd0ba5d898bebd233b51dc9f576
snak3ater <saeedyasir91@gmail.com>
2015-04-27 13:32:08 +0500

    Turn art optimization to everything
    Original commit: https://github.com/Chroma-Aosp/android_vendor_chroma/commit/a8ecd5b2e4724448ccf92b29e08eb1a249e21957

--- configs/common.mk
+++ configs/common.mk
@@ -19,7 +19,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.facelock.rec_timeout=2500 \
     ro.facelock.lively_timeout=2500 \
     ro.facelock.est_max_time=600 \
-    ro.facelock.use_intro_anim=false
+    ro.facelock.use_intro_anim=false \
+    dalvik.vm.image-dex2oat-filter=everything \
+    dalvik.vm.dex2oat-filter=everything
 
 # Common overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/simpleaosp/overlays/common
