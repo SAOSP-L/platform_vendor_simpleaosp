a723f9198b86d2f2472f25a2a418e928045de182
snak3ater <saeedyasir91@gmail.com>
2015-06-21 10:37:08 +0500

    Remove TMobile themes permission
    * https://github.com/CyanogenMod/android_vendor_cm/commit/9d782d9c19d6a3a1d4198f34eda80da241ae39a6#diff-9ddec1e800192d7377790f5adcbc2ceaL12

--- configs/themes_common.mk
+++ configs/themes_common.mk
@@ -4,5 +4,4 @@ PRODUCT_PACKAGES += \
     ThemesProvider
 
 PRODUCT_COPY_FILES += \
-       vendor/simpleaosp/configs/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
        vendor/simpleaosp/configs/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
